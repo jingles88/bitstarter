c564cce6bc0028751db2c2748903a3f4f0677a66
James Inglis <jamesldinglis@gmail.com>
2015-04-29 14:26:13 +0000

    added info to index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send("The Dream - #russisafartbag");
+  response.send("The Dream - #jingisking #feeling5again");
 });
 
 app.listen(app.get('port'), function() {
