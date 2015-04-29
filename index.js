d69643d17a042d913516ff79c1a6297a6eee267a
James Inglis <jamesldinglis@gmail.com>
2015-04-29 09:38:36 +0000

    changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send("HellowWorld2");
+  response.send("The Dream - #russisafartbag");
 });
 
 app.listen(app.get('port'), function() {
