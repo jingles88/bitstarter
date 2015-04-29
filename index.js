e00d7587f6b212b62cc926fb4dbe5178c20efdce
James Inglis <jamesldinglis@gmail.com>
2015-04-29 09:24:59 +0000

    changes

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-fs = fs.require('fs')
-
 app.get('/', function(request, response) {
   response.send("HellowWorld2");
 });
