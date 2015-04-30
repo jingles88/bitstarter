e79d43fc34f2948e53021acfccc1d94c68e9609d
James Inglis <jamesldinglis@gmail.com>
2015-04-30 09:30:33 +0000

    readfile attempt

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require('express')
 var app = express();
 
+// Load the fs (filesystem) module
+var fs = require('fs');
+
+// Read the contents of the file into memory.
+fs.readFile('example_log.txt', function (err, logData) {
+  
+// If an error occurred, throwing it will
+  // display the exception and end our app.
+  if (err) throw err;
+});
+
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send("Whyyyyyyyyy");
+  response.send(fs);
 });
 
 app.listen(app.get('port'), function() {
