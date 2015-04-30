8d6d0ac79579581b4a16b8cde12e226f016f55af
James Inglis <jamesldinglis@gmail.com>
2015-04-30 13:53:05 +0000

    try 50

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(x);
+  response.send(data);
 });
 
 app.listen(app.get('port'), function() {
