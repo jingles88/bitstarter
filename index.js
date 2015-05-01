84b315935688bd25bf42c031b75c00b05ba0d723
James Inglis <jamesldinglis@gmail.com>
2015-05-01 16:03:51 +0000

    all replaced to original

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-var express = require('express')
+var express = require('express');
 var app = express();
-var fs = require('fs');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-fs.readFile('/home/ubuntu/bitstarter/index.html', 'utf8' , function (err,data)
-{
-if (err) throw err;
-});
-//app.get('/', function(request, response) {
-//response.send(data);
-//}
-//});
-
-//app.set('port', (process.env.PORT || 5000))
-//app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-response.send("blank");
+  response.send('Hello World!');
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log('Node app is running on port', app.get('port'));
+});
