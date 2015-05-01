53a0bfef7c7040114d037c2662378a3d0223ad45
James Inglis <jamesldinglis@gmail.com>
2015-05-01 16:38:59 +0000

    :(

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
+var x;
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+fs.readFile('index.html','utf8', function (err, data) {
+  if (err) throw err;
+  x=data;
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(x);
 });
 
 app.listen(app.get('port'), function() {
