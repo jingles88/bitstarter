4d471a6c757e469c41041de18dd75af30c03af0f
James Inglis <jamesldinglis@gmail.com>
2015-05-05 18:13:26 +0000

    bitstarter webstie from coursera

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 fs.readFile('index.html','utf8', function (err, data) {
-  if (err) throw err;
-  x=data;
+ if (err) throw err;
+ x=data;
 });
 
 app.get('/', function(request, response) {
